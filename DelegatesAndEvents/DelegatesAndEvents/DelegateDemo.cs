3c487892da4fb5d0f107bc38be0882fb3c0366de
“VenuReddy773” <venu706@gmail.com>
2021-11-24 14:46:51 +0530

    [VenuGopalReddy] Added: Delegate And Events Demo Program

---
+++ DelegatesAndEvents/DelegatesAndEvents/DelegateDemo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegatesAndEvents
+{
+    public delegate int operations(int x, int y);
+    class DelegateDemo
+    {
+        public static int Addition(int a,int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ DelegatesAndEvents/DelegatesAndEvents/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DelegatesAndEvents
+{
+    class Program
+    {
+        public static event DelEventHandler Add;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Delegates And Events program!!\n");
+            operations obj = new operations(DelegateDemo.Addition); // : Single Delegate.
+            Console.WriteLine("Single Delegate!!...Addition is {0}\n",obj(25,25));
+            Console.WriteLine("MultiCast Delegates...\n");
+            MultiCastDelegate.ImplementDelegate();          // : MultiCast Delegate.
+            Console.WriteLine("Events....\n");
+            Add += new DelEventHandler(EventsDemo.India);   // : Events.
+            Add += new DelEventHandler(EventsDemo.Florida);
+            Add += new DelEventHandler(EventsDemo.Usa);
+            Add.Invoke();
+            
+        }
+    }
+}
